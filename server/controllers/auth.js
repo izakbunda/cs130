bb63699316dca5f83c5677446992ed4ef7d33ae9
Izak Bunda <izakbunda@gmail.com>
2024-10-28 13:28:35 -0700

    created backend server connected to mongodb with functional authentication

---
+++ server/controllers/auth.js
@@ -0,0 +1,75 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+
+// send the new user back if everything goes well
+export const register = async (req, res) => {
+  try {
+    const { email, password, profile } = req.body;
+
+    const salt = await bcrypt.genSalt(); // used to encrypt password
+    console.log("Generated salt:", salt);
+    console.log(email);
+    console.log("Password", password);
+
+    const passwordHash = await bcrypt.hash(password, salt);
+    console.log("Password hash:", passwordHash);
+
+    const newUser = new User({
+      // set values from req body
+      email,
+      password: passwordHash,
+      profile: {
+        username: profile.username || "defaultusername",
+        picturePath: profile.picturePath || "",
+      },
+
+      // default values:
+      folders: [], // array -> empty
+      pets: [], // array -> empty
+      createdAt: Date.now(),
+    });
+
+    console.log("New user to be saved:", newUser);
+
+    const savedUser = await newUser.save();
+
+    // send the new user back with status 201 (Created)
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    // send error 500 (Internal Server)
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* LOGGIN IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // we are using mongoose to try to find the user with this specified user
+    const user = await User.findOne({ email: email });
+
+    // correct email
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist. " });
+    }
+
+    // correct password
+    const isMatch = await bcrypt.compare(password, user.password); // will use the same salt
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials. " });
+    }
+
+    // JWT token setup
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password; // delete so it doesnt getsent to the frontend
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,69 @@
+import express from "express";
 import mongoose from "mongoose";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path"; // built into node
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+
+// import { verifyToken } from "./middleware/auth.js";
+
+const __filename = fileURLToPath(import.meta.url); // so we can grab the file url
+const __dirname = path.dirname(__filename);
+dotenv.config(); // this allows us to do process.env
+
+const app = express();
+app.use(express.json()); // allows us to JSON payloads in request bodies
+
+/* security */
+
+// cors - https://www.npmjs.com/package/helmet
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // need this to avoid cors error
+app.use(cors());
+
+/* debugging help */
+
+// for reading http errors - https://expressjs.com/en/resources/middleware/morgan.html
+app.use(morgan("common")); // 'common' outputs method, URL, status, and response time of request
+
+/* Debugging Middleware */
+app.use((req, res, next) => {
+  console.log("Request Headers:", req.headers);
+  console.log("Request Body:", req.body);
+  next();
+});
+
+/* requests */
+// will parse incoming request bodies as JSON
+app.use(bodyParser.json({ extended: true })); // extended: true  will allow us to parse nested objects
+// will parse incoming bodies from URL
+app.use(bodyParser.urlencoded({ extended: true })); // extended: true  will allows us to parse nested objects
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 6001;
+// app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+console.log("Connecting to MongoDB with URL:", process.env.MONGO_URL);
+mongoose
+
+  .connect(process.env.MONGO_URL, {
+    // useNewURLParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD THIS ONE TIME */
+    // manually inject this information
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
