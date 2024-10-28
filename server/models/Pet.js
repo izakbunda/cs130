1a574d09b81c22000fe396654f8e308092675ed9
Izak Bunda <izakbunda@gmail.com>
2024-10-28 13:23:21 -0700

    tweaked Task, User, and Pet models and created new Folder and Notes models

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -11,20 +11,9 @@ const PetSchema = new mongoose.Schema({
     required: true,
   },
   name: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ["alive", "dead"],
-    default: "alive",
-  },
-  points: { type: Number, default: 0 }, // current points
-  birthDate: { type: Date, default: Date.now },
-  deathDate: { type: Date }, // opional: only set if the pet dies
-  level: { type: Number, default: 1 }, // pet's current level based on points
-  evolutionStage: {
-    type: String,
-    enum: ["baby", "child", "teen", "adult"],
-    default: "baby",
-  },
+
+  points: { type: Number, default: 0 }, // current xp
+  level: { type: Number, default: 0 }, // pet's current level based on points
 });
 
 const Pet = mongoose.model("Pet", PetSchema);
