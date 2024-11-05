06f14dd5a6ad18eb9d745fa9d4e3cd1777d2434e
Scott Park <scottpark03@gmail.com>
2024-11-05 13:54:06 -0800

    Clock Component
    Created clock.jsx and clock.css to dynamically style/create a clock component to be used on the Landing Page. Also additionaly changes to index.css

---
+++ client/src/screens/landing-page.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "../css/index.css";
+import Clock from '../components/clock'
+function Landing(){
+return(
+    <html>
+        <h1> Todogotchi </h1>
+        <Clock />
+    </html>
+)
+}
+export default Landing;
