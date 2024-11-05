4491bd389f7683d378a772e1f3c46defcebf8e61
Scott Park <scottpark03@gmail.com>
2024-11-05 02:20:13 -0800

    Hierarchy, Styling, & Format
    Deleted uneccessary css files and modularized the filesystem with a components, css, and screens folder. Established the style sheet in index.css that can be used throughout the entire application with standardized fonts, sizes, colors, viewport settings, etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./css/index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <html>
+      <h1>
+        TODOGOTCHI
+      </h1>
+      <h2>
+        h2 Sharkie
+      </h2>
+        
+      <h3>
+      h3 Todogotchi
+      </h3>
+      <h4>
+        h4 Log In
+
+      </h4>
+      <h5>
+        h5 Sunny Weather Date
+      </h5>
+      <h6>
+        h6 Due In
+      </h6>
       <p>
-        Todogotchi by Dylan Chan, Izak Bunda, Scott Park, and Ying Chou for COM
-        SCI 130
+        p Notes
       </p>
-    </>
+    </html>
   );
 }
 
