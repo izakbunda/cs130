6c7e089e2e0ec8544a1491f4d17259679ab53a7d
Scott Park <scottpark03@gmail.com>
2024-11-05 21:45:03 -0800

    Weather Component
    Used OpenWeatherMap API to display the weather on Landing Page. Made weather.jsx and weather.css which uses geolocation, the weather API, and various functional components to retrieve and display data. Additionally made a row-grid component using GridLayour from react-grid-layout

--- client/src/screens/landing-page.jsx
+++ client/src/screens/landing-page.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "../css/index.css";
 import Clock from '../components/clock'
+import Weather from "../components/weather";
+import InfoGrid from "../components/row-grid";
 function Landing(){
 return(
     <html>
         <h1> Todogotchi </h1>
-        <Clock />
+        {/* <Clock />
+        <Weather /> */}
+        <InfoGrid />
+        
+        
     </html>
 )
 }
