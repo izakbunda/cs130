724c16b5f31859505dd6ce06b216989b152a910c
Scott Park <scottpark03@gmail.com>
2024-11-06 11:34:23 -0800

    Animation to Pet
    added a shadow to the pet as well as a periodic jumping animation using useEffect and keyFrames

--- client/src/components/pet.jsx
+++ client/src/components/pet.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/pet.css';
 
 const PetIcon = ({ name, level, exp, page }) => {
-  
+  const [isJumping, setIsJumping] = useState(false);
+
   let petImage;
+  let isSharkieC = false;
   if (level >= 0 && level <= 8) {
     petImage = '../../public/SharkieA.png'; 
   } else if (level >= 9 && level <= 12) {
     petImage = '../../public/SharkieB.png'; 
   } else if (level >= 13) {
     petImage = '../../public/SharkieC.png';
+    isSharkieC = true;
   }
 
-  const sizeClass =
-    page === 'Landing' ? 'pet-large' :  'pet-small'
+  useEffect(() => {
+    if (isSharkieC) {
+      const interval = setInterval(() => {
+        setIsJumping(true);
+        setTimeout(() => setIsJumping(false), 1000);
+      }, 8000); 
+
+      return () => clearInterval(interval);
+    }
+  }, [isSharkieC]);
+
+  const sizeClass = page === 'Landing' ? 'pet-large' : 'pet-small';
 
   return (
     <div className={`pet-container ${sizeClass}`}>
-      <img src={petImage} alt={`Pet - ${name}`} className="pet-image" />
+      <div className="pet-shadow"></div>
+      <img
+        src={petImage}
+        alt={`Pet - ${name}`}
+        className={`pet-image ${isJumping ? 'jump' : ''}`} 
+      />
       <h2 className="pet-name">{name}</h2>
       <h5 className="pet-level">Level: {level}</h5>
     </div>
