e5f63fa4ea921beaa0d8eac0ac6b42c27cf63eb9
MrDychi <dylanrchan@gmail.com>
2024-12-01 14:11:45 -0800

    added comments

--- src/renderer/client/src/components/dateTime.jsx
+++ src/renderer/client/src/components/dateTime.jsx
@@ -1,34 +1,41 @@
+// Import native stuff
 import React, { useState } from 'react';
 
 function DateTimePicker({ top, left, defaultDate, defaultTime, title, onPress, actionOnInvalid }) {
+    // format information before storing in local states
     const providedDate = defaultDate ? defaultDate : '';
     const providedTime = defaultTime ? defaultTime : '';
 
+    // Local States
     const [date, setDate] = useState(providedDate);
     const [time, setTime] = useState(providedTime);
     
+    // update data when date is changed
     const handleDateChange = (e) => {
         setDate(e.target.value);
     };
 
+    // update data when time is changed
     const handleTimeChange = (e) => {
         setTime(e.target.value);
     };
 
+    // when submit button is pressed, alert when invalid data
     const handleSubmit = () => {
         if(date && time) {
-            onPress(date+"T"+time+":00")
+            onPress(date+"T"+time+":00");
         } else if (date) {
-            console.log("no time")
-            alert("No time was set")
-            actionOnInvalid()
+            console.log("no time");
+            alert("No time was set");
+            actionOnInvalid();
         } else {
             console.log("no date")
-            alert("No date was set")
-            actionOnInvalid()
+            alert("No date was set");
+            actionOnInvalid();
         }
-    }
+    };
 
+    // return progress bar UI
     return (
         <div className='due-date-menu' style={{ top: top, left: left }}>
             <label >New Date</label>
@@ -49,6 +56,6 @@ function DateTimePicker({ top, left, defaultDate, defaultTime, title, onPress, a
             <button className='date-submit-button'onClick={handleSubmit}>Set Date</button>
         </div>
     );
-}
+};
 
 export default DateTimePicker;
